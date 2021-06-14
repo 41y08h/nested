a911af6af20e1a36d8568f1fd4885807fded6421
Piyush <piyush41y08h@gmail.com>
2021-06-14 16:28:27 +0530

    feat: add and view todo

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,57 @@
+import {
+  Container,
+  Spinner,
+  ListGroup,
+  Form,
+  InputGroup,
+  Button,
+} from "react-bootstrap";
+import { useMutation, useQuery } from "react-query";
+import ITodo from "./interfaces/Todo";
+import { FormEventHandler, useRef } from "react";
+import axios from "axios";
+
 function App() {
-  return <div>hey</div>;
+  const { isLoading, data } = useQuery<ITodo[]>("/todos");
+  const inputRef = useRef<HTMLInputElement>(null);
+  const todosMutation = useMutation((text: string) =>
+    axios.post("/todos", { text }).then((res) => res.data)
+  );
+
+  const handleSubmit: FormEventHandler = (e) => {
+    e.preventDefault();
+    const input = inputRef.current;
+    if (input?.value) todosMutation.mutate(input.value);
+  };
+
+  return (
+    <Container>
+      <h1 className="text-center my-4">Todos</h1>
+      <div className="col-6 mx-auto my-4">
+        {isLoading && <Spinner animation="border" />}
+        {data && (
+          <ListGroup.Item>
+            {data.map((todo) => (
+              <ListGroup.Item>{todo.text}</ListGroup.Item>
+            ))}
+          </ListGroup.Item>
+        )}
+        <Form onSubmit={handleSubmit}>
+          <InputGroup className="mb-3 my-2">
+            <Form.Control
+              ref={inputRef}
+              required
+              type="text"
+              placeholder="Add a new todo..."
+            />
+            <Button variant="dark" type="submit">
+              Add
+            </Button>
+          </InputGroup>
+        </Form>
+      </div>
+    </Container>
+  );
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
+import { QueryClientProvider, QueryClient } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+import axios from "axios";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      async queryFn(ctx) {
+        const [url] = ctx.queryKey;
+        const { data } = await axios(url as string);
+        return data;
+      },
+    },
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
