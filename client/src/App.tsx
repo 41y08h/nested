42e41081a74dfd50ae4b3c926b6dec1d597b4c66
Piyush <piyush41y08h@gmail.com>
2021-06-16 10:44:37 +0530

    Add prettier config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,63 +5,60 @@ import {
   ListGroup,
   Container,
   InputGroup,
-} from "react-bootstrap";
-import axios from "axios";
-import Todo from "./components/Todo";
-import ITodo from "./interfaces/Todo";
-import Loading from "./components/Loading";
-import AsyncData from "./components/AsyncData";
-import { FormEventHandler, useRef } from "react";
-import { useMutation, useQuery, useQueryClient } from "react-query";
+} from 'react-bootstrap'
+import axios from 'axios'
+import Todo from './components/Todo'
+import ITodo from './interfaces/Todo'
+import Loading from './components/Loading'
+import AsyncData from './components/AsyncData'
+import { FormEventHandler, useRef } from 'react'
+import { useMutation, useQuery, useQueryClient } from 'react-query'
 
-type TInput = HTMLInputElement;
-const key = "/todos";
+type TInput = HTMLInputElement
+const key = '/todos'
 
 export default function App() {
-  const queryClient = useQueryClient();
-  const inputRef = useRef<TInput>(null);
-  const todos = useQuery<ITodo[]>(key);
+  const queryClient = useQueryClient()
+  const inputRef = useRef<TInput>(null)
+  const todos = useQuery<ITodo[]>(key)
 
   const todosMutation = useMutation(async (text: string) => {
-    const { data } = await axios.post<ITodo>(key, { text });
-    return data;
-  });
-
-  const handleSubmit: FormEventHandler = async (event) => {
-    event.preventDefault();
-    const input = inputRef.current;
+    const { data } = await axios.post<ITodo>(key, { text })
+    return data
+  })
+  const handleSubmit: FormEventHandler = async event => {
+    event.preventDefault()
+    const input = inputRef.current
 
     // Terminate early with falsy values
-    if (!input?.value.trim()) return;
+    if (!input?.value.trim()) return
 
     // Make network request and update the query data
-    const data = await todosMutation.mutateAsync(input.value);
-    queryClient.setQueryData<ITodo[]>(key, (old) =>
-      old ? [...old, data] : []
-    );
+    const data = await todosMutation.mutateAsync(input.value)
+    queryClient.setQueryData<ITodo[]>(key, old => (old ? [...old, data] : []))
 
-    input.value = "";
-  };
+    input.value = ''
+  }
 
   const handleTodoDeleted = (deletedId: number) => {
     // Remove the item from the query data instantly
-    queryClient.setQueryData<ITodo[]>(key, (old) =>
-      old ? old.filter((todo) => todo.id !== deletedId) : []
-    );
-  };
+    queryClient.setQueryData<ITodo[]>(key, old =>
+      old ? old.filter(todo => todo.id !== deletedId) : []
+    )
+  }
 
   const handleTodoEdited = (updated: ITodo) => {
     // Update the item from the query data instantly
-    queryClient.setQueryData<ITodo[]>(key, (old) =>
-      old ? old.map((todo) => (todo.id === updated.id ? updated : todo)) : []
-    );
-  };
+    queryClient.setQueryData<ITodo[]>(key, old =>
+      old ? old.map(todo => (todo.id === updated.id ? updated : todo)) : []
+    )
+  }
 
   return (
     <Container>
       <div className="mx-auto my-4 mt-5 p-4 rounded main-container">
         <AsyncData isLoading={todos.isLoading} data={todos.data}>
-          {(data) => (
+          {data => (
             <div>
               {!data.length && (
                 <div className="text-center">
@@ -74,7 +71,7 @@ export default function App() {
                 </div>
               )}
               <ListGroup className="rounded-0">
-                {data.map((todo) => (
+                {data.map(todo => (
                   <Todo
                     key={todo.id}
                     todo={todo}
@@ -103,12 +100,12 @@ export default function App() {
               {todosMutation.isLoading ? (
                 <Spinner animation="border" size="sm" />
               ) : (
-                "Add"
+                'Add'
               )}
             </Button>
           </InputGroup>
         </Form>
       </div>
     </Container>
-  );
+  )
 }
