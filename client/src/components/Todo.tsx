9c9edaab8516d4e8df33bbf462436a6d32ef7254
Piyush <piyush41y08h@gmail.com>
2021-06-16 11:09:34 +0530

    Replace `Button` component

--- client/src/components/Todo.tsx
+++ client/src/components/Todo.tsx
@@ -1,43 +1,37 @@
-import axios from "axios";
-import { FC, FormEventHandler, useState, useRef } from "react";
-import { useMutation } from "react-query";
-import ITodo from "../interfaces/Todo";
-import {
-  ListGroup,
-  Button,
-  Spinner,
-  ButtonGroup,
-  Form,
-  InputGroup,
-} from "react-bootstrap";
+import axios from 'axios'
+import ITodo from '../interfaces/Todo'
+import Button from '../components/Button'
+import { useMutation } from 'react-query'
+import { FC, FormEventHandler, useState, useRef } from 'react'
+import { ListGroup, ButtonGroup, Form, InputGroup } from 'react-bootstrap'
 
 interface Props {
-  todo: ITodo;
-  onDeleted: (id: number) => any;
-  onEdited: (todo: ITodo) => any;
+  todo: ITodo
+  onDeleted: (id: number) => any
+  onEdited: (todo: ITodo) => any
 }
 
 const Todo: FC<Props> = ({ todo, onDeleted, onEdited }) => {
-  const inputRef = useRef<HTMLInputElement>(null);
-  const [isEditing, setIsEditing] = useState(false);
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [isEditing, setIsEditing] = useState(false)
   const deleteMutation = useMutation(() => axios.delete(`/todos/${todo.id}`), {
     onSuccess: () => onDeleted(todo.id),
-  });
+  })
   const editMutation = useMutation((text: string) =>
-    axios.patch(`/todos/${todo.id}`, { text }).then((res) => res.data)
-  );
+    axios.patch(`/todos/${todo.id}`, { text }).then(res => res.data)
+  )
 
-  const toggleIsEditing = () => setIsEditing((old) => !old);
+  const toggleIsEditing = () => setIsEditing(old => !old)
 
-  const handleSubmit: FormEventHandler = async (e) => {
-    e.preventDefault();
-    const input = inputRef.current;
-    if (!input?.value) return;
+  const handleSubmit: FormEventHandler = async e => {
+    e.preventDefault()
+    const input = inputRef.current
+    if (!input?.value) return
 
-    const todo = await editMutation.mutateAsync(input.value);
-    toggleIsEditing();
-    onEdited(todo);
-  };
+    const todo = await editMutation.mutateAsync(input.value)
+    toggleIsEditing()
+    onEdited(todo)
+  }
 
   return (
     <ListGroup.Item className="d-flex justify-content-between align-items-center">
@@ -46,16 +40,12 @@ const Todo: FC<Props> = ({ todo, onDeleted, onEdited }) => {
           <InputGroup>
             <Form.Control ref={inputRef} defaultValue={todo.text} required />
             <Button
-              disabled={editMutation.isLoading}
+              isLoading={editMutation.isLoading}
               variant="primary"
               type="submit"
               className="px-4"
             >
-              {editMutation.isLoading ? (
-                <Spinner animation="border" size="sm" />
-              ) : (
-                "⬆"
-              )}
+              ⬆
             </Button>
           </InputGroup>
         </Form>
@@ -67,19 +57,15 @@ const Todo: FC<Props> = ({ todo, onDeleted, onEdited }) => {
           🖊
         </Button>
         <Button
-          disabled={deleteMutation.isLoading}
+          isLoading={deleteMutation.isLoading}
           variant="light"
           onClick={() => deleteMutation.mutate()}
         >
-          {deleteMutation.isLoading ? (
-            <Spinner animation="border" size="sm" />
-          ) : (
-            "❌"
-          )}
+          ❌
         </Button>
       </ButtonGroup>
     </ListGroup.Item>
-  );
-};
+  )
+}
 
-export default Todo;
+export default Todo
