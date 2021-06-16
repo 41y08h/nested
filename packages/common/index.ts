6ece1ce62bf1d47853201a227d779cc436664403
Piyush <piyush41y08h@gmail.com>
2021-06-16 12:58:40 +0530

    Use yarn workspaces

---
+++ packages/common/index.ts
@@ -0,0 +1,1 @@
+export default 'hello string from common'
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,16 +1,18 @@
-import express from "express";
-import createAPIRouter from "./lib/createAPIRouter";
+import express from 'express'
+import createAPIRouter from './lib/createAPIRouter'
+import common from 'common'
 
 async function main() {
-  const app = express();
+  console.log(common)
+  const app = express()
 
-  app.use(express.json());
+  app.use(express.json())
 
-  const api = createAPIRouter();
-  app.use(api);
+  const api = createAPIRouter()
+  app.use(api)
 
-  app.listen(5000);
-  console.log("ready on port 5000");
+  app.listen(5000)
+  console.log('ready on port 5000')
 }
 
-main();
+main()
